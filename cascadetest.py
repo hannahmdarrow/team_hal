4fd214350410f6c97d546ee08de95bc5e77c78f5
facedetecshin <105749172+facedetecshin@users.noreply.github.com>
2022-05-18 11:16:59 -0400

    Update cascadetest.py
    Circle, Press D to record value, Value is recorded in tesla variable

--- cascadetest.py
+++ cascadetest.py
@@ -1,16 +1,33 @@
 from __future__ import print_function
 import cv2 as cv
 import argparse
+import keyboard
+
+tesla = (0,0,0,0)
+
 def detectAndDisplay(frame):
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame_gray = cv.equalizeHist(frame_gray)
+    
     #-- Detect faces
     faces = face_cascade.detectMultiScale(frame_gray)
+    
     for (x,y,w,h) in faces:
         center = (x + w//2, y + h//2)
         frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
         faceROI = frame_gray[y:y+h,x:x+w]
-        #-- In each face, detect eyes
+
+        try:  # used try so that if user pressed other than the given key error will not be shown
+            if keyboard.is_pressed('D'):  # if key 'q' is pressed 
+                print('You Pressed A Key!')
+                tesla = (x,y,h,w)
+                print(x,y,h,w)
+                
+                break  # finishing the loop
+        except:
+            break  # if user pressed a key other than the given key the loop will break
+            
+        
     cv.imshow('Capture - Face detection', frame)
 parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
 parser.add_argument('--face_cascade', help='Path to face cascade.', default='haarcascade_frontalface_alt.xml')
@@ -18,11 +35,13 @@ parser.add_argument('--camera', help='Camera divide number.', type=int, default=
 args = parser.parse_args()
 face_cascade_name = args.face_cascade
 face_cascade = cv.CascadeClassifier()
+
 #-- 1. Load the cascades
 if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
     print('--(!)Error loading face cascade')
     exit(0)
 camera_device = args.camera
+
 #-- 2. Read the video stream
 cap = cv.VideoCapture(camera_device)
 if not cap.isOpened:
