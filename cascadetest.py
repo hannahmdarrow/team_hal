a898f6d262a6fedaae615177e5b40712b27280c5
facedetecshin <105749172+facedetecshin@users.noreply.github.com>
2022-05-19 17:48:44 -0400

    commented out comparingbaseline
    we dont use that no more

--- cascadetest.py
+++ cascadetest.py
@@ -5,34 +5,34 @@ import keyboard
 
 baselineValue = (0,0,0,0)
 currentValue = (0,0,0,0)
-def comparingBaseline(baselineValue, currentValue):
-    x = currentValue[0]
-    y = currentValue[1]
-    w = currentValue[2]
-    h = currentValue[3]
+#def comparingBaseline(baselineValue, currentValue):
+#    x = currentValue[0]
+#    y = currentValue[1]
+#    w = currentValue[2]
+#    h = currentValue[3]
 
-    upperHigh = 0 #establishing too far threshold
-    upperLow = 0#too close threshold
-    baselineVal = 0
-    currentVal = 0#what is coming live from the feed
-    
-    for z in baselineValue:#turning baseline tuple into a numerical value
-        baselineVal += z
-
-    if baselineVal != 0:
-        for x in currentValue:
-            upperHigh += x
-            upperLow += x
-
-        upperHigh += upperHigh*.01
-        upperLow -= upperLow*.02
-    
-        if upperHigh < baselineVal:
-            print("Too FAR!!")
-        if upperLow > baselineVal:
-            print("TOO Close!!")
-
-
+    #upperHigh = 0 #establishing too far threshold
+    #upperLow = 0#too close threshold
+    #baselineVal = 0
+    #currentVal = 0#what is coming live from the feed
+   # 
+  #  for z in baselineValue:#turning baseline tuple into a numerical value
+ #       baselineVal += z
+#
+    #if baselineVal != 0:
+   #     for x in currentValue:
+  #          upperHigh += x
+ #           upperLow += x
+#
+      #  upperHigh += upperHigh*.01
+     #   upperLow -= upperLow*.02
+    #
+    #    if upperHigh < baselineVal:
+   #         print("Too FAR!!")
+  #      if upperLow > baselineVal:
+ #           print("TOO Close!!")
+#
+#
 def detectAndDisplay(frame, baselineValue, currentValue, face_cascade):#main camera loop function
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame_gray = cv.equalizeHist(frame_gray)
