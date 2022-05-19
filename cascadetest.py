852bf5b04238bffa5098be1a0d17b04b8bbaeb94
kelleyli <kelley.li@anavationllc.com>
2022-05-18 20:55:57 -0400

    kept the functions in cascade test, need to run main.py to call them

--- cascadetest.py
+++ cascadetest.py
@@ -33,30 +33,3 @@ def detectAndDisplay(frame, baselineValue):#main camera loop function
     cv.imshow('Capture - Face detection', frame)
     return None, baselineValue
 
-parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-parser.add_argument('--face_cascade', help='Path to face cascade.', default='haarcascade_frontalface_alt.xml')
-parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
-args = parser.parse_args()
-face_cascade_name = args.face_cascade
-face_cascade = cv.CascadeClassifier()
-
-#-- 1. Load the cascades
-if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
-    print('--(!)Error loading face cascade')
-    exit(0)
-camera_device = args.camera
-
-#-- 2. Read the video stream
-cap = cv.VideoCapture(camera_device)
-if not cap.isOpened:
-    print('--(!)Error opening video capture')
-    exit(0)
-while True:
-    ret, frame = cap.read()
-    if frame is None:
-        print('--(!) No captured frame -- Break!')
-        break
-    _, baselineValue = detectAndDisplay(frame, baselineValue)
-    if cv.waitKey(1) & 0xFF == ord('q'):#program closes when q is pressed.
-        break
-cv.destroyAllWindows()
